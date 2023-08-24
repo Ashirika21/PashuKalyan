bd0c94a825a5a61099188e9ff44c7fd243104b9a
Ashirika Bhardwaj <ashirikabhardwaj@gmail.com>
2023-08-25 00:26:29 +0530

    Release Project

---
+++ navigator.js
@@ -0,0 +1,5 @@
+navigator.geolocation
+Geolocation {}
+navigator.geolocation.getCurrentPosition(console.log, console.log)
+GeolocationPosition{coords: GeolocationCoordinates, timestamp: 1580737682491}
+
